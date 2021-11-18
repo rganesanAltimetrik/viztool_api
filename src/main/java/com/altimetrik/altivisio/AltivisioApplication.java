257a3ce396d18562b7ee9fe1499f8824e5e49860
Bharathi Raja K <Braja4375@altimetrik.com>
2021-11-18 17:28:25 +0530

    first

---
+++ src/main/java/com/altimetrik/altivisio/AltivisioApplication.java
@@ -0,0 +1,13 @@
+package com.altimetrik.altivisio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltivisioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltivisioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/altimetrik/altivisio/AltivisioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altimetrik.altivisio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltivisioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
