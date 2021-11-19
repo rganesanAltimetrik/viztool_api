9dc3f091f0c6952fdf166a277414dca320459235
Braja4375 <bharathiraja85@gmail.com>
2021-11-19 19:49:15 +0530

    Added sample rest template call for reusage

--- src/main/java/com/altimetrik/altivisio/scheduler/ReportingScheduler.java
+++ src/main/java/com/altimetrik/altivisio/scheduler/ReportingScheduler.java
@@ -2,9 +2,16 @@ package com.altimetrik.altivisio.scheduler;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Date;
 
 
@@ -13,9 +20,17 @@ public class ReportingScheduler {
 
     Logger log = LoggerFactory.getLogger(ReportingScheduler.class);
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     //Cron Expression runs on every Friday and 1'O clock and 14 minutes
-    @Scheduled(cron = "0 0/14 13 * * FRI")
+    @Scheduled(cron = "0 0/47 19 * * FRI")
     private void generate(){
         log.info("Into Logger scheduler"+new Date());
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+
+        log.info(restTemplate.exchange("https://gorest.co.in/public/v1/users", HttpMethod.GET, entity, String.class).getBody());
     }
 }
